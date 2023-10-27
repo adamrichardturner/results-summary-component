21a68068fcc708cc1aab315f85c19e361aa33f26
adamrichardturner <hello@adamrichardturner.dev>
2023-10-27 15:27:51 +0100

    Added alt tag to images

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
-const summaryContainer = document.querySelector(".summary__stats__wrapper")
-fetch('https://raw.githubusercontent.com/adamrichardturner/results-summary-component/main/data.json')
-    .then(response => response.json())
-    .then(data => {
-        const dataMap = data.map((feature) => {
-            return `
+const summaryContainer = document.querySelector(".summary__stats__wrapper");
+fetch(
+  "https://raw.githubusercontent.com/adamrichardturner/results-summary-component/main/data.json"
+)
+  .then((response) => response.json())
+  .then((data) => {
+    const dataMap = data
+      .map((feature) => {
+        return `
                 <div class="summary__stats__wrapper__${feature.category.toLowerCase()}">
                     <div class="summary__stats__wrapper__${feature.category.toLowerCase()}-title">
-                        <img src="${feature.icon}" />
+                        <img src="${feature.icon}" alt="${feature.category}" />
                         <h3>${feature.category}</h3>
                     </div>
                     <div class="summary__stats__wrapper__${feature.category.toLowerCase()}-detail">
                         <h3>${feature.score}<span> / 100</span></h3>
                     </div>
                 </div>
-            `
-        }).join("")
-        summaryContainer.innerHTML = dataMap
-    })
+            `;
+      })
+      .join("");
+    summaryContainer.innerHTML = dataMap;
+  });
